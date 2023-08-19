b52f2443c0000dd9aa1cc68289a18bd60b5c0518
Roman <jrj80mail@gmail.com>
2023-08-19 16:07:14 +0300

    install libraries

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+//? npm install simplelightbox
+//? npm install notiflix
+//? npm install scrollmonitor
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import ApiService from './api-service';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+import scrollMonitor from 'scrollmonitor';
